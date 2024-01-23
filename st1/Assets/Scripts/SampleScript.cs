1625959de46dacad9e9d48a45ea46e149d90a19b
yeatsg <yeatsg@gmail.com>
2024-01-23 17:35:43 -0500

    WebGL + new Scripts
    For unity learn publishing and writing new scripts for project

---
+++ st1/Assets/Scripts/SampleScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Helllo worldd, it's mee, Emphazimaa. I needd helpp.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
